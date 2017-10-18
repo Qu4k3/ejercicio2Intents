26df152754ee7e689c75d2afccd906275493e373
Santi <sdetord@lasalle.cat>
2017-10-18 15:54:57 +0200

    Code on the way

--- app/src/main/java/com/example/sw/ejerciciointents/MenuActivity.java
+++ app/src/main/java/com/example/sw/ejerciciointents/MenuActivity.java
@@ -1,13 +1,34 @@
 package com.example.sw.ejerciciointents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText etQuantity;
+    //private TextView tvMaxQuantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        etQuantity = (EditText) findViewById(R.id.menu_choose_quantity);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.menu_buy){
+            if(etQuantity.getText().toString().isEmpty()){
+                //empty -> return
+                return;
+            }
+            int quantity = Integer.valueOf(etQuantity.getText().toString());
+            Intent intent = new Intent(MenuActivity.this, PurchaseActivity.class);
+
+        }
     }
 }
